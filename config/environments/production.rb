725f3ba63f27a02791ccf36ccb2e043ab97fe933
numa-create <k.numata0815@gmail.com>
2021-12-07 13:51:12 +0900

    apiドメイン修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,5 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.hosts << 'tranquil-spire-25969'
+  config.hosts << 'tranquil-spire-25969.herokuapp.com'
 end
