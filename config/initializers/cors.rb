e656d9c0d53a23e21e97160be68a8b03c6456fda
numa-create <k.numata0815@gmail.com>
2021-12-07 14:19:49 +0900

    heroku再設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://numa-create.github.io'
+    origins '127.0.0.1:5500'
     resource '*', headers: :any, methods: %i[get post patch put]
   end
 end
