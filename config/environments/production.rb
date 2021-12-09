af8024daa93e9c267124e303ea2e09b558a05fe5
numa-create <k.numata0815@gmail.com>
2021-12-09 13:18:20 +0900

    フロント調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,5 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.hosts << 'peaceful-tundra-91273.herokuapp.com'
+  config.hosts << 'guarded-sea-65851.herokuapp.com'
 end
