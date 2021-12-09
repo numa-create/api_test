8d3964aeb19514ea08abcc2f21b5c06930774141
numa-create <k.numata0815@gmail.com>
2021-12-09 13:24:47 +0900

    CORSとホストの設定を追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '127.0.0.1:5501'
+    origins 'https://numa-create.github.io'
     resource '*', headers: :any, methods: %i[get post patch put]
   end
 end
